61bca50876d559bf336144e3ab99d6b2b16046a9
nkwon0815 <46616321+nkwon0815@users.noreply.github.com>
2020-05-19 10:33:49 -0700

    Update contact_me.php

--- contact_me.php
+++ contact_me.php
@@ -10,7 +10,7 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "nathan.quan03@gmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = "ikoo03@hotmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Website Contact Form:  $name";
 $header = "From: $email"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 mail($to, $subject, $message, $header);
